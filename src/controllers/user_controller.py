7c88132ba04f145436d235b71bb91d1600055b36
orisabiyi <orisabiyidavid@gmail.com>
2025-08-21 01:05:46 +0100

    feat: Enhance user creation endpoint with password hashing and error handling

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,15 +1,32 @@
+from typing import Optional
 from sqlmodel import SQLModel, Field
 from src.database import SessionDep
+from passlib.context import CryptContext
 from fastapi import APIRouter
 
 router = APIRouter()
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 class User(SQLModel, table=True):
-    user_id: str = Field(primary_key=True)
+    user_id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True)
-    email: str = Field(index=True)
+    email: str = Field(index=True, unique=True)
+    password: str = Field()
 
 
 @router.post('/create/user')
 async def create_user(user: User, session: SessionDep):
-    return {"message": "User created successfully!"}
+    try:
+        hash_password = pwd_context.hash(user.password)
+        user.password = hash_password
+        
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+        return {"message": "User created successfully!", "user_id": user.user_id}
+    
+    except Exception as e:
+        session.rollback()
+        return {"error": str(e)}
